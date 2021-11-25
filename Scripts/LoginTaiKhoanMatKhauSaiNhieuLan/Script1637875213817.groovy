31ecf624416136e47f8e10bfa76240adc852e86f
vangiaurecca <vangiau.recca@gmail.com>
2021-11-26 05:17:57 +0700

    Upload 26/11/2021

---
+++ Scripts/LoginTaiKhoanMatKhauSaiNhieuLan/Script1637875213817.groovy
@@ -0,0 +1,43 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('https://online.hcmue.edu.vn/')
+
+WebUI.click(findTestObject('Page_UP Portal  HoChiMinh University of Pedagogy/lbtDangNhap'))
+
+WebUI.setText(findTestObject('Page_UP Portal  HoChiMinh University of Pedagogy/input_txtUserName'), 'aksdkjasdkasdj')
+
+WebUI.setText(findTestObject('Page_UP Portal  HoChiMinh University of Pedagogy/input_txtPassword'), 'sjfksjfkdjsgkjsdf')
+
+WebUI.click(findTestObject('Page_UP Portal  HoChiMinh University of Pedagogy/btLogin'))
+
+WebUI.setText(findTestObject('Page_UP Portal  HoChiMinh University of Pedagogy/input_txtUserName'), 'qoiuweiruoiwe')
+
+WebUI.setText(findTestObject('Page_UP Portal  HoChiMinh University of Pedagogy/input_txtPassword'), 'zcbzxcbnaskdlasjd')
+
+WebUI.click(findTestObject('Page_UP Portal  HoChiMinh University of Pedagogy/btLogin'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.setText(findTestObject('Page_UP Portal  HoChiMinh University of Pedagogy/input_txtUserName'), 'ydsflsdflksdf')
+
+WebUI.setText(findTestObject('Page_UP Portal  HoChiMinh University of Pedagogy/input_txtPassword'), 'dfsdjalkalksd')
+
+WebUI.click(findTestObject('Page_UP Portal  HoChiMinh University of Pedagogy/btLogin'))
+
+WebUI.closeBrowser()
+
---
+++ Test Suites/Login.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>38862b9e-9afd-4ba1-8b92-f94529f7082f</testSuiteGuid>
+   <testCaseLink>
+      <guid>1907ba9c-0213-4e83-9920-776e950bfebf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/LoginTaiKhoanHopLe</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
